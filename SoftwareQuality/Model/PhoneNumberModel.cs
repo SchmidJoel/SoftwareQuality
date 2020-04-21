ec625c8e77c11363247bdce1daeb93ccf0310905
GrafZeppelin <jojo98.hannes@gmail.com>
2020-04-21 16:20:10 +0200

    add Phone Number Test

--- SoftwareQuality/Model/PhoneNumberModel.cs
+++ SoftwareQuality/Model/PhoneNumberModel.cs
@@ -11,5 +11,15 @@
         public string ParticipantNumber;
 
         public string Extension;
+
+        public override bool Equals(object obj)
+        {
+            var number = obj as PhoneNumberModel;
+            return CountryCode.Equals(number.CountryCode) &&
+                ISOCountryText.Equals(number.ISOCountryText) &&
+                AreaCode.Equals(number.AreaCode) &&
+                ParticipantNumber.Equals(number.ParticipantNumber) &&
+                Extension.Equals(number.Extension);
+        }
     }
 }
